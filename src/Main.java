bb76f537e82f0120b772dd5ee419edfd7e6a959e
Your Name <you@example.com>
2023-01-03 13:58:15 +0200

    home work without default constructor

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Person personInfo = new Person("Jack", "Captian", 33);
+        Person personGender = new Person("Jack", "male");
+        Person personAllField = new Person("Jack", "Captian",33,"Male","Kuba_sea");
+
+
+
+
+
+
+
+
+
+    }
+}
